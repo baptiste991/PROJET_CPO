1410878770498aaf66c9cb11524ce6719e90a751
Baptiste AUBRY <baptiste.aubry@epfedu.fr>
2023-11-13 18:15:40 +0100

    Création d'un nouveau projet versionné :

---
+++ PROJET_LABYRINTHE/src/projet_labyrinthe/PROJET_LABYRINTHE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_labyrinthe;
+
+/**
+ *
+ * @author bapti
+ */
+public class PROJET_LABYRINTHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
