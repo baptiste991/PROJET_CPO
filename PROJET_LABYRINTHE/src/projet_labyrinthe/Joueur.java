2a27510102f3a952901155a1ccc52a99821625ba
Ulysse Long <ulysse.long@epfedu.fr>
2023-11-15 15:41:04 +0100

    Ajout des méthodes déplacement de la classe Joueur

--- PROJET_LABYRINTHE/src/projet_labyrinthe/Joueur.java
+++ PROJET_LABYRINTHE/src/projet_labyrinthe/Joueur.java
@@ -7,4 +7,60 @@ package projet_labyrinthe;
  */
 public class Joueur {
     
+    /**
+     * @see posx
+     *      Variable qui représente la position x sur le plateau du joueur
+     * @see posy
+     *      Variable qui représente la position y sur le plateau du joueur
+     */
+    
+    private int posx, posy;
+    
+    /**
+     * Methode qui permet le déplacement d'un joueur au Nord de la carte
+     * Il faudra rajouter des procédures pour vérifier si ce déplacement 
+     * est possible
+     * @see posy
+     *          La position sur y du joueur sur le plateau
+     */
+    public void deplacementNord(){
+        posy++;
+    }
+    
+     /**
+     * Methode qui permet le déplacement d'un joueur au Sud de la carte
+     * Il faudra rajouter des procédures pour vérifier si ce déplacement 
+     * est possible
+     * @see posy
+     *          La position sur y du joueur sur le plateau
+     */
+    public void deplacementSud(){
+        posy--;
+    }
+    
+     /**
+     * Methode qui permet le déplacement d'un joueur à l'Est de la carte
+     * Il faudra rajouter des procédures pour vérifier si ce déplacement 
+     * est possible
+     * @see posx
+     *          La position sur x du joueur sur le plateau
+     */
+    public void deplacementEst(){
+        posx++;
+    }
+    
+     /**
+     * Methode qui permet le déplacement d'un joueur à l'Ouest de la carte
+     * Il faudra rajouter des procédures pour vérifier si ce déplacement 
+     * est possible
+     * @see posx
+     *          La position sur x du joueur sur le plateau
+     */
+    public void deplacementOuest(){
+        posx--;
+    }
+    
+    
+
+    
 }
