a9d1b614e858e06d519112c225faa678f62fde6d
Ulysse Long <ulysse.long@epfedu.fr>
2023-11-18 14:43:16 +0100

    Classe Settings : Ajout Methode setAllMissions

--- PROJET_LABYRINTHE/src/projet_labyrinthe/Settings.java
+++ PROJET_LABYRINTHE/src/projet_labyrinthe/Settings.java
@@ -1,6 +1,7 @@
 package projet_labyrinthe;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Cette classe a pour but d'initialiser les paramètres de la partie comme :
@@ -15,6 +16,15 @@ public abstract class Settings {
      * @see listeDeJoueurs
      */
     private ArrayList<Joueur> listeDeJoueurs = new ArrayList();
+    
+    /** 
+     * La liste des objets sous forme de tableau de String
+     * @see listeDesObjets
+     */
+    private String [] listeDesObjets = {"Princess","Gobelin","Helmet","Emerald",
+        "Lizard","Trophy","Rat","Keys","Crown","Gold","Chest","Genius","Ghost",
+        "Painting","Drake","Spider"};
+    
     
     /**
      * Methode qui met à jour la liste des joueurs de la partie
@@ -77,6 +87,88 @@ public abstract class Settings {
      */
     public ArrayList<Joueur> getListeDeJoueurs(){
         return listeDeJoueurs;
+    }
+    
+    /**
+     * Methode qui initialise toutes les missions  i.e associe à chaque objet
+     * (représenté par une chaine de caractère) un joueur pour le récupérer
+     * @return
+     *     Un ArrayList de Mission ; il y a autant de missions que d'objets
+     */
+    public ArrayList<Mission> setAllMissions(){
+        
+        ArrayList<Mission> allMissions = new ArrayList();
+        
+        // Remarque : Le switch suivant fait apparaitre beaucoup de code surement factorisable, il serait interessant
+        // de trouver une façon de le condenser, mais actuellement on l'utilise.
+        switch(this.listeDeJoueurs.size()){
+            
+            case 1->{
+                for(int i=0;i<this.listeDesObjets.length;i++){
+                    Mission mission = new Mission(this.listeDeJoueurs.get(0), this.listeDesObjets[i]);
+                    allMissions.add(mission);
+                }
+            }
+                
+            case 2 ->{
+                Random random = new Random();
+                
+                ArrayList<String> objetsRestants = new ArrayList();
+                for(int i=0;i<this.listeDesObjets.length;i++){
+                    objetsRestants.add(this.listeDesObjets[i]);
+                }
+                                
+                for(int j=0;j<2;j++){
+                    for(int i=0;i<this.listeDesObjets.length/2;i++){
+                        int indexRandom = random.nextInt(objetsRestants.size());
+                        Mission mission = new Mission(this.listeDeJoueurs.get(j), objetsRestants.get(indexRandom));
+                        allMissions.add(mission);
+                        objetsRestants.remove(indexRandom);
+                    }
+                }
+            }    
+            
+            case 3 ->{
+                Random random = new Random();
+                
+                ArrayList<String> objetsRestants = new ArrayList();
+                for(int i=0;i<this.listeDesObjets.length;i++){
+                    objetsRestants.add(this.listeDesObjets[i]);
+                }
+                
+                objetsRestants.remove(random.nextInt(objetsRestants.size()));
+                // On supprime un objet car 3 joueurs => this.listeDesObjets.length/3 pas entier
+                
+                for(int j=0;j<3;j++){
+                    for(int i=0;i<objetsRestants.size()/3;j++){
+                        int indexRandom = random.nextInt(objetsRestants.size());
+                        Mission mission = new Mission(this.listeDeJoueurs.get(j), objetsRestants.get(indexRandom));
+                        allMissions.add(mission);
+                        objetsRestants.remove(indexRandom);
+                    }
+                }
+            }
+            
+            case 4->{
+                Random random = new Random();
+                
+                ArrayList<String> objetsRestants = new ArrayList();
+                for(int i=0;i<this.listeDesObjets.length;i++){
+                    objetsRestants.add(this.listeDesObjets[i]);
+                }
+                
+                for(int j=0;j<4;j++){
+                    for(int i=0;i<objetsRestants.size()/3;j++){
+                        int indexRandom = random.nextInt(objetsRestants.size());
+                        Mission mission = new Mission(this.listeDeJoueurs.get(j), objetsRestants.get(indexRandom));
+                        allMissions.add(mission);
+                        objetsRestants.remove(indexRandom);
+                    }
+                }
+            }
+            
+        }
+        return allMissions;
     }
     
 }
