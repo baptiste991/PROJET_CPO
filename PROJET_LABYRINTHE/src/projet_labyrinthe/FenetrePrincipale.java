b1ce4e5e4653762a2a61f926ab34c7af11f45f01
Baptiste AUBRY <baptiste.aubry@epfedu.fr>
2023-12-23 15:49:01 +0100

    ajout ébauche fenetre fin de partie

--- PROJET_LABYRINTHE/src/projet_labyrinthe/FenetrePrincipale.java
+++ PROJET_LABYRINTHE/src/projet_labyrinthe/FenetrePrincipale.java
@@ -207,7 +207,7 @@ public class FenetrePrincipale extends javax.swing.JFrame {
        switch(this.plateau.getListeDeJoueurs().size()){
             case 1->{
                 if(this.ordre.size()==0){
-                FenetreVictoire win = new FenetreVictoire(podium);
+                FenetreFinPartie win = new FenetreFinPartie(podium);
                 this.dispose();
                 }
             }
@@ -216,7 +216,7 @@ public class FenetrePrincipale extends javax.swing.JFrame {
                 
                 if(this.ordre.size()==1){
                     podium.add(ordre.get(0));
-                    FenetreVictoire win = new FenetreVictoire(podium);
+                    FenetreFinPartie win = new FenetreFinPartie(podium);
                     for(int i=0;i<this.ordre.size();i++){
                         ordre.remove(i);
                     }
